f0f8a8c3ff7120c3088e6b38bb86c3b37850ecac
pourya pooryeganeh <pour.pourya1999@gmail.com>
2025-04-04 13:58:01 +0330

    .

--- main.py
+++ main.py
@@ -3,3 +3,35 @@ import torch
 from PIL import Image
 
 model, preprocess = clip.load("ViT-B/32", device = "cuda")
+
+from pathlib import Path
+import requests
+
+wiki_titles = [
+    "RoboCop",
+    "Labour Party (UK)",
+    "SpaceX",
+    "OpenAI",
+]
+
+data_path = Path("data_wiki")
+
+for title in wiki_titles:
+    response = requests.get(
+        "https://en.wikipedia.org/w/api.php",
+        params={
+            "action": "query",
+            "format": "json",
+            "titles": title,
+            "prop": "extracts",
+            "explaintext": True,
+        },
+    ).json()
+    page = next(iter(response["query"]["pages"].values()))
+    wiki_text = page["extract"]
+
+    if not data_path.exists():
+        Path.mkdir(data_path)
+
+    with open(data_path / f"{title}.txt", "w", encoding="utf-8") as fp:
+        fp.write(wiki_text)
